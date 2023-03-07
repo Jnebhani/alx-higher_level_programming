3f25225cb40e6377b6cf3e81bcfc3439cc3d357d
Jibril Nebhani <abbakotif@gmail.com>
2023-03-06 18:25:08 -0800

    0-run

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -10,12 +10,12 @@
  *
  * Description: singly linked list node structure
  * for Holberton project
- */ 
+ */
 typedef struct listint_s
 {
-        int n;
-        struct listint_s *next;
-}       
+int n;
+struct listint_s *next;
+}
 listint_t;
 size_t print_listint(const listint_t *h);
 listint_t *add_nodekint(listint_t **head, const int n);
