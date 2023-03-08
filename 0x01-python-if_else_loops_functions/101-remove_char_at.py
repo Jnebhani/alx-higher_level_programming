9e4cbabd438ca49c718392944a088277c9cf520c
Jibril Nebhani <abbakotif@gmail.com>
2023-03-07 18:44:40 -0800

    101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,12 +1,8 @@
-#!/usr/bin/python3
-# 102-magic_calculation.py
-# Brennan D Baraban <375@holbertonschool.com>
+#!/usr/bin//python3
+#101-remove_char_at.py
 
-
-def magic_calculation(a, b, c):
-    """Match bytecode provided by Holberton School."""
-    if a < b:
-        return (c)
-    if c > b:
-        return (a + b)
-    return (a*b - c)
+def remove_char_at(str,n):
+    """ Create a copy of the string without the character at position n."""
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
