18e7b5bf64feaebb7c777f116bad849c1f34fa97
Jibril Nebhani <abbakotif@gmail.com>
2023-03-15 17:53:30 -0700

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,5 +2,4 @@
 def common_elements(set_1, set_2):
    set_1 = ['banana','orange','apple']
    set_2 = ['mango','apple']
-   b_set = common_elements(set_1, set_2)
-   print(sorted(list(b_set)))
+   return set_1 & set_2
