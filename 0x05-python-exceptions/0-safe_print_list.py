3ff3c51bf7aa8a12d65d3965196e2d000f84032c
Jibril Nebhani <abbakotif@gmail.com>
2023-03-27 19:00:01 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     y = 0
-    for i in range(x):
+    x > y
+    for y n range(x):
         try:
             print("{}".format(my_list[i]), end='')
             y += 1
