6d4d9bcbff78284f253954bbba9d129440fdc39e
Jibril Nebhani <abbakotif@gmail.com>
2023-04-03 18:00:11 -0700

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""0-rectangle, built for My project 0x08 task of 0.
+"""
+
+
+class Rectangle:
+    """Empty class per task instructions, will be built upon in later tasks.
+    """
+    pass
