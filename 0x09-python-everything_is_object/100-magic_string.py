560597a8a49b1a2c20c349ae0c4e12123af37936
Jibril Nebhani <abbakotif@gmail.com>
2023-04-04 19:39:26 -0700

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-magic_string(“BestSchool” ):
-    for i in range(n)
-    print(magic_string)
+def magic_string:
+    setattr(magic_string, "d", getattr(magic_string, "d", -1) + 1)
+     return "Bestschool" + (", Bestschool" * magic_string.d)
