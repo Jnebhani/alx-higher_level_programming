76f6de7d011b988381b014a835c405294892f4f1
Jibril Nebhani <abbakotif@gmail.com>
2023-04-11 06:16:05 -0700

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,42 +1,14 @@
 #!/usr/bin/python3
+
 """
-Module for log parsing scripts.
+add_attribute Module
 """
 
 
-import sys
-
-
-if __name__ == "__main__":
-    size = [0]
-    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-
-    def check_match(line):
-        '''Checks for regexp match in line.'''
-        try:
-            line = line[:-1]
-            words = line.split(" ")
-            size[0] += int(words[-1])
-            code = int(words[-2])
-            if code in codes:
-                codes[code] += 1
-        except ValueError:
-            pass
-
-    def print_stats():
-        '''Prints accumulated statistics.'''
-        print("File size: {}".format(size[0]))
-        for k in sorted(codes.keys()):
-            if codes[k]:
-                print("{}: {}".format(k, codes[k]))
-    i = 1
-    try:
-        for line in sys.stdin:
-            check_match(line)
-            if i % 10 == 0:
-                print_stats()
-            i += 1
-    except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
+def add_attribute(obj, key, value):
+    """Adds a new attribute to the object"""
+    if not hasattr(obj, "__slots__") and not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    if hasattr(obj, "__slots__") and not hasattr(obj, key):
+        raise TypeError("can't add new attribute")
+    setattr(obj, key, value)
