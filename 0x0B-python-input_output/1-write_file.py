24c64be16b835b8d89a4544efe13424bd4ca1947
Jibril Nebhani <abbakotif@gmail.com>
2023-04-11 16:48:41 -0700

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def number_of_lines(filename=""):
-    """ function that returns the number of lines of a text file """
 
-    with open(filename) as f:
-        count = len(f.readlines())
-    return count
+
+def read_file(filename=""):
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read(), end="")
