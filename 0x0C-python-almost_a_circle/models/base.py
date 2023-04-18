a37064a0edac510be2e080c03fabde0a7236e85c
Jibril Nebhani <abbakotif@gmail.com>
2023-04-18 05:11:13 -0700

    models/base.py, models/__init__.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Defining the class
+Class Base:
+    # private class attribute
+    self.__nb_objects = 0
+    # class constructor: 
+        def __init__(self,id=None):
+            if id != None:
+               self.id = int()
+           else:
+               self.id=new value 
+               __nb_objects ++ 
