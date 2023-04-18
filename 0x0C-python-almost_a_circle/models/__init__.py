0e4379335e97a37f0c498cd420da152e6cb66646
Jibril Nebhani <abbakotif@gmail.com>
2023-04-18 11:17:02 -0700

    base.py

--- 0x0C-python-almost_a_circle/models/__init__.py
+++ 0x0C-python-almost_a_circle/models/__init__.py
@@ -1,22 +1,2 @@
-def update(self, *args, **kwargs):
-        '''
-        Makes args variadic
-        '''
-        argc = len(args)
-        if argc > 0:
-            try:
-                self.id = args[0]
-                self.size = args[1]
-                self.x = args[3]
-                self.y = args[4]
-            except IndexError:
-                pass
-        else:
-            if 'id' in kwargs:
-                self.id = kwargs['id']
-            if 'size' in kwargs:
-                self.size = kwargs['size']
-            if 'x' in kwargs:
-                self.x = kwargs['x']
-            if 'y' in kwargs:
-                self.y = kwargs['y']
+#!/usr/bin/pyhton3
+-
